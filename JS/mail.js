ba9b3622733e7c1caa7c3b08c34a529abc1872f3
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-08-19 17:12:10 -0400

    finta login

---
+++ JS/mail.js
@@ -0,0 +1,10 @@
+/*------------------------
+ESERCIZIO 2 - Finta Login
+-------------------------
+Inventa una lista di email autorizzate
+Chiedi all’utente la sua email, con un piccolo form.
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Cosa devo controllare esattamente? */
