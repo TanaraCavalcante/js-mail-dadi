bb0ea8641807cbb1ed26d4992f16dc0e3b6df5c3
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-08-19 17:35:46 -0400

    mail - preparazione

--- JS/mail.js
+++ JS/mail.js
@@ -1,10 +1,25 @@
 /*------------------------
 ESERCIZIO 2 - Finta Login
--------------------------
+--------------------------
 Inventa una lista di email autorizzate
 Chiedi all’utente la sua email, con un piccolo form.
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
+
 Prima di partire a scrivere codice poniamoci qualche domanda:
 Che ci sia un array da qualche parte?
 Cosa devo controllare esattamente? */
+
+//TODO       FASE PREPARATORIA
+//Ricupero gli ellementi della pagina di mio interesse
+
+//TODO       FASE DE ELABORAZIONE
+//inventa una lista di email autorizzate
+
+//Chiedi all’utente la sua email, con un piccolo form.
+
+//TODO       CONTROLLO
+//controlla che sia nella lista di chi può accedere.
+
+//TODO       OUTPUT
+//stampa un messaggio appropriato sull’esito del controllo.
