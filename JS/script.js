d993cf7d358cf5ceacb892bc5fe85d3cbf54101c
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-08-19 16:18:52 -0400

    Montagio esercizio 1

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+/*----------------------------
+ESERCIZIO 1 - Gioco dei dadi
+------------------------------
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Stampiamo in pagina i due tiri e il risultato
+*/
+
+//TODO       FASE PREPARATORIA
+//Ricupero gli ellementi della pagina di mio interesse
+
+//TODO       FASE DE ELABORAZIONE
+//Generare un numero random da 1 a 6
+
+//TODO       CALCOLO
+//Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+//TODO       OUTPUT
+//Stampiamo in pagina i due tiri e il risultato
