65fe08ac234ff1d00b246b14fe6a131e51bf5451
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-08-20 16:22:56 -0400

    dadi ottimizzato!

--- JS/dadi.js
+++ JS/dadi.js
@@ -9,26 +9,29 @@ Stampiamo in pagina i due tiri e il risultato
 //TODO       FASE PREPARATORIA
 //Ricupero gli ellementi della pagina di mio interesse
 const result = document.getElementById('result');
+const dadi = document.getElementById('dadi');
 
 //TODO       FASE DE ELABORAZIONE
 //Generare un numero random da 1 a 6
 const playerNumber = Math.floor(Math.random()*6)+1;
 const pcNumber = Math.floor(Math.random()*6)+1;
 
+let dadiResult = `<p> il valore del giocatore è uguale a : ${playerNumber} </p>
+                  <p> il valore del Computer è uguale a : ${pcNumber} </p>`
+
 //TODO       CALCOLO
 //Stabilire il vincitore, in base a chi fa il punteggio più alto.
-let message = '';
+let message = 'Entrambi sono uguali, non ha vinto nessuno dei due!';
 
 if(playerNumber > pcNumber ){
-    message += `${playerNumber} è magiore che ${pcNumber}, ha vinto il Giocatore!`
+    message =  'Ha vinto il Giocatore!'
 }else if(pcNumber > playerNumber){
-    message += `${pcNumber} è magiore che ${playerNumber}, ha vinto il Computer!`
-}else{
-    message += 'Entrambi sono uguali, non ha vinto nessuno dei due!'
+    message = 'Ha vinto il Computer!'
 }
 console.log(message);
 //TODO       OUTPUT
 //Stampiamo in pagina i due tiri e il risultato
-result.innerText = message
+dadi.innerHTML = dadiResult;
+result.innerText = message;
 
 
